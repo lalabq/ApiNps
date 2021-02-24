d785915ecef0ac482d75fdd552158d5e626f149a
Lavinea Sens <lavineasens@hotmail.com>
2021-02-23 22:04:50 -0300

    Criação de rota - create user

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,11 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database';
+import { router } from "./routes";
 
 const app = express();
 
-/**
- * Métodos HTTP:
- * 
- * GET    => Busca
- * POST   => Salvar
- * PUT    => Alterar
- * DELETE => Apagar
- * PATCH  => Alteração específica
- */
-
-// http://localhost:3333/users
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello, world" });
-});
-
-app.post("/", (request, response) => {
-  return response.json({ message: "Os dados foram salvos com sucesso!" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
